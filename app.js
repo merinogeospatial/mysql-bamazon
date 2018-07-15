d00a679147a847dc65d607cf10b9fbcdb797ac62
R <merinogeospatial@gmail.com>
2018-07-15 16:54:38 -0400

    checkpoint

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
+// Dependencies
 const inquirer = require("inquirer");
 const mysql = require("mysql");
 
-let db = mysql.createConnection({
+// Initialize MySQL connection
+const db = mysql.createConnection({
   host     : 'localhost',
   port     : 8889,
   user     : 'root',
@@ -11,8 +13,40 @@ let db = mysql.createConnection({
  
 db.connect();
  
-db.query('SELECT * FROM products ', function (error, results, fields) {
-  if (error) throw error;
-  console.log(results);
-});
-db.end();
+
+
+// Define subroutines
+function start() {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'chosenID',
+            message: 'Please enter the ID of the product you wish to purchase:'
+        },
+        {
+            type: 'input',
+            name: 'chosenQuantity',
+            message: 'Please enter how many you would like to purchase:'
+        }
+    ]).then(function(answer){
+        console.log(answer.chosenID, answer.chosenQuantity);
+
+        //---- Let's put to handle ID matching, checking if there is enough, then loggin the total of their transaction
+        //  We will need to query to return a list of objects, then do a for loop + if statement to catch a matching id.
+        // OR a better way would be to use a ? wild card in the query and have answer.chosenID fulfull that ? query
+        // it would look something like dbquer('SELECT * FROM products WHERE item_id = ?', answer.chosenID, function(err,res){}...
+
+
+
+        // db.query('SELECT * FROM products ', function (error, results, fields) {
+        //   if (error) throw error;
+        //   console.log(results);
+        // });
+        // db.end();
+
+
+    })
+}
+
+start();
+
