004eb395f38349601bfa3123449ec9dd2b82e64e
R <merinogeospatial@gmail.com>
2018-07-20 20:21:00 -0400

    complete logic

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ db.connect();
 
 // Define subroutines
 function start() {
+
     inquirer.prompt([
         {
             type: 'input',
@@ -29,40 +30,60 @@ function start() {
             message: 'Please enter how many you would like to purchase:'
         }
     ]).then(function(answer){
-        console.log(answer.chosenID, answer.chosenQuantity);
         getProduct(answer.chosenID,answer.chosenQuantity);
-       
-
-        //---- Let's put to handle ID matching, checking if there is enough, then loggin the total of their transaction
-        //  We will need to query to return a list of objects, then do a for loop + if statement to catch a matching id.
-        // OR a better way would be to use a ? wild card in the query and have answer.chosenID fulfull that ? query
-        // it would look something like dbquer('SELECT * FROM products WHERE item_id = ?', answer.chosenID, function(err,res){}...
-
-
-
-        // db.query('SELECT * FROM products ', function (error, results, fields) {
-        //   if (error) throw error;
-        //   console.log(results);
-        // });
-        // db.end();
-
-
     });
 }
 
 function getProduct(chosenID,quantity) {
+
     db.query(
-        'UPDATE products SET stock_quantity = (stock_quantity - ? ) WHERE item_id = ?', [quantity,chosenID], function(err, res) {
-            console.log("You bought " + quantity + " of item ID: " + chosenID);
+        'SELECT stock_quantity, price FROM products WHERE item_id = ?', chosenID, function(err, res) {
+            if (res[0].stock_quantity < quantity) {
+                console.log("Sorry, we don't have enough!");
+            }
+            else {
+                db.query(
+                    'UPDATE products SET stock_quantity = (stock_quantity - ? ) WHERE item_id = ?', [quantity,chosenID], function(err, res) {
+                        console.log("You bought " + quantity + " of item ID: " + chosenID);
+                    }
+                )
+                console.log("The total price is " + (parseFloat(res[0].price) * parseFloat(quantity)));
+            }
         }
     )
 
-    db.query(
-        'SELECT price FROM products WHERE item_id = ?', chosenID, function(err, res) {
-            console.log("The total price is " + (parseFloat(res[0].price) * parseFloat(quantity)));
-        }
-    )
+    setTimeout(showTable,1000);
 }
 
+
+
+function showTable() {
+    var Table = require('cli-table');
+ 
+// instantiate
+var table = new Table({
+    head: ['id', 'product', 'department', 'stock']
+  , colWidths: [5, 20, 20,20]
+});
+ 
+// table is an Array, so you can `push`, `unshift`, `splice` and friends
+    db.query(
+        'SELECT * FROM products', function(err,res) {
+
+        for (let i = 0; i < res.length; i++) {
+            
+            table.push(
+                [res[i].item_id.toString(), res[i].product_name.toString(), res[i].department_name.toString(), res[i].stock_quantity.toString()]
+            );
+        } 
+
+console.log("                       ");
+console.log(table.toString());
+console.log("                       ");
 start();
 
+});
+}
+
+
+showTable();
